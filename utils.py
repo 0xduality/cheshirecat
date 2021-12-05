d8d524281b4c27687ed47c0ca48f88976c7dccd4
avax moon <onthedouble@protonmail.com>
2021-12-04 23:08:23 -0800

    make abis more readable and move to dedicated dir

--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
 import json
-import re
 import requests
-from bs4 import BeautifulSoup
+import os
 
 def get_abi(addy):
-    cache = f'{addy}.json'
+    home = os.path.expanduser("~")
+    cache_dir = os.path.join(home, '.abicache')
+    cache = os.path.join(cache_dir, f'{addy}.json')
     try:
         with open(cache) as inp:
             abi = json.loads(inp.read())
     except Exception as e:
         print(e)
         resp = requests.get(f'https://api.snowtrace.io/api?module=contract&action=getabi&address={addy}')
-        abi = resp.json()['result']
+        abi = json.loads(resp.json()['result'])
+        os.makedirs(cache_dir, exist_ok=True)
         with open(cache, 'w') as out:
             out.write(json.dumps(abi, indent=True))
     return abi
+
 
 def get_contract(w3, addy):
     return w3.eth.contract(address=addy, abi=get_abi(addy))
